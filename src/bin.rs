c713b87eb953356ade5cfa0f0237e672efa13d78
Stefan Kaufhold <cere@fastmail.fm>
2018-02-25 03:00:55 +0100

    redo CLI

--- src/bin.rs
+++ src/bin.rs
@@ -1,41 +1,72 @@
-extern crate twitch_archiver;
 extern crate diesel;
-extern crate dotenv;
 extern crate error_chain;
+#[macro_use] extern crate quicli;
+extern crate twitch_archiver;
+extern crate core;
 
 use diesel::Connection;
 use diesel::pg::PgConnection;
 use error_chain::ChainedError;
-use std::env;
+use quicli::prelude::*;
 use std::fs::File;
-use std::io::{BufReader, Write};
-use std::path::Path;
+use std::io::{BufReader};
+use std::path::PathBuf;
 use twitch_archiver::collector::PgCollector;
 use twitch_archiver::parser::{ChattyParser, LogParser};
+use core::str::FromStr;
 
-fn connect() -> PgConnection {
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+
+/// Parse chatty log files and save the data in other formats or databases
+#[derive(Debug, StructOpt)]
+struct Cli {
+    /// Output/Exporting format (pg, csv)
+    #[structopt(long = "output", short = "o")]
+    format: Output,
+    /// Database URL if saving in a database
+    #[structopt(long = "database", short = "db")]
+    db_url: Option<String>,
+    /// The log file to read
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
 }
 
-fn main() {
-    ::dotenv::dotenv().ok();
-    let args: Vec<String> = env::args().collect();
-    let path = Path::new(args.get(1)
-        .expect("Path parameter required"));
+#[derive(Debug)]
+enum Output {
+    Pg,
+    Csv,
+}
 
-    if !path.exists() { panic!("File does not exist") };
-    if !path.is_file() { panic!("Path is not a file") };
+impl FromStr for Output {
+    type Err = Error;
 
-    println!("Opening log file {}", args[1]);
-    let file = File::open(path)
-        .expect("Couldn't open file");
-
-    let connection = connect();
-    let mut collector = PgCollector::new(&connection);
-    if let Err(err) = ChattyParser::parse(&mut collector, BufReader::new(file)) {
-        writeln!(&mut ::std::io::stderr(), "{}", err.display_chain()).unwrap();
+    fn from_str(s: &str) -> Result<Self> {
+        match s {
+            "pg" | "postgres" => Ok(Output::Pg),
+            "csv" => Ok(Output::Csv),
+            _ => bail!("Invalid output type")
+        }
     }
 }
+
+main!(|args: Cli| {
+    if !args.file.exists() { bail!("File does not exist") };
+    if !args.file.is_file() { bail!("Path is not a file") };
+
+    println!("Opening log file {:?}", args.file);
+    let file = File::open(args.file)?;
+
+    match args.format {
+        Output::Pg => {
+            let connection = PgConnection::establish(
+                args.db_url
+                    .ok_or_else(|| format_err!("Database URL missing"))?
+                    .as_ref()
+            )?;
+            let mut collector = PgCollector::new(&connection);
+            if let Err(err) = ChattyParser::parse(&mut collector, BufReader::new(file)) {
+                error!("{}", err.display_chain());
+            }
+        },
+        Output::Csv => {}
+    }
+});
